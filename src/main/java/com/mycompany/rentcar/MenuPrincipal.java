07469955b84ba2c67da95279f728f4ffaad9a526
User <User@DESKTOP-2MA536R>
2026-03-19 20:48:13 -0400

    estoy viendo que hace

--- src/main/java/com/mycompany/rentcar/MenuPrincipal.java
+++ src/main/java/com/mycompany/rentcar/MenuPrincipal.java
@@ -9,10 +9,10 @@ package com.mycompany.rentcar;
  * la pampara de la pieza
  yo soy la pieza
  ningunniengun
+ * eso estaba mal escrito
  */
 public class MenuPrincipal {
 
-  public static void main(String[] args) {
-        System.out.println("Hello World!");
+  
     
 }
